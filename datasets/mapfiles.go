0c82cea68dfcaf94317fe466a8a88d22c129ed7a
Shashi Bhuhan Rai <24620309+shashi-rai@users.noreply.github.com>
2022-01-06 07:32:35 +0530

    Create mapfiles.go

---
+++ datasets/mapfiles.go
@@ -0,0 +1,10 @@
+package datasets
+
+var MapFileNames = map[string]string{
+	"india":    "india",
+	"world":    "world",
+	"बिहार":      "bihar",
+	"पटना":      "patna",
+	"भोजपुर":     "bhojpur",
+  "आरा":       "bhojpur",
+}
